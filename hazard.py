c86314c57d4e9f2177671be6b5b0eee4af9ff2be
Olof Sjödin <me@olofsjodin.se>
2017-10-24 18:46:53 +0200

    add board class

--- hazard.py
+++ hazard.py
@@ -1,11 +1,37 @@
-
 VERSION = "0.1"
+class Board:
+    BOARD_X_WIDTH = 10
+    BOARD_Y_WIDTH = 20
+    NULL_BLOCK = 0
+    
+    def __init__(self):
+        # Build tetris board with origo at top left corner
+        # Assume that x and y are the horizontal and vertical respectively
+        # Assume that x=10 and y=20
+        # Let self.board[x][y] contain the block
+        
+        self.board = []
+        
+        for x in range(self.BOARD_X_WIDTH):
+            self.board.append([])
+            for y in range(self.BOARD_Y_WIDTH):
+                self.board[x].append(self.NULL_BLOCK)
+
+    def printBoard(self):
+        for x_row in self.board:
+            temp = ""
+            for y_col in x_row:
+                temp += str(y_col)
+            print(temp)
+        
 
 def main():
     print("Starting Hazard Game Server " + VERSION +
           ", built by Olof Sjödin and/or members of Qnarch")
-    
 
+    b = Board()
+
+    b.printBoard()
 
 if __name__ == "__main__":
     main()
