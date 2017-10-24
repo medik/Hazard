8cdf1254438c31fbb2d6577fd6271163b566d7bc
Olof Sjödin <me@olofsjodin.se>
2017-10-24 18:08:39 +0200

    add skelethon

---
+++ hazard.py
@@ -0,0 +1,11 @@
+
+VERSION = "0.1"
+
+def main():
+    print("Starting Hazard Game Server " + VERSION +
+          ", built by Olof Sjödin and/or members of Qnarch")
+    
+
+
+if __name__ == "__main__":
+    main()
