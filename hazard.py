e136032638d7976b252d8210ac4575332c7774c9
Olof Sjödin <me@olofsjodin.se>
2017-10-29 15:21:31 +0100

    use movement class in main

--- hazard.py
+++ hazard.py
@@ -1,13 +1,24 @@
-from tetrislib import Board
+from tetrislib import Board, Movement
 VERSION = "0.1"
 
 def main():
     print("Starting Hazard Game Server " + VERSION +
           ", built by Olof Sjödin and/or members of Qnarch")
 
+    print("NOTE: Starting test scenario (this will NOT be in the final version)")
+    input()
+
+    queue = []
+
+    queue.append(Movement("hard_drop"))
+    queue.append(Movement("right"))
+    queue.append(Movement("right"))
+
     b = Board()
     b.setActiveBlockFromString("S-block")
-    for i in range(41):
+
+    for item in queue:
+        b.applyAction(item)
         b.update()
 
 
